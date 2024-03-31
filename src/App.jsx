8fc21b825fe11b180dee954ceffc620c5920f733
Esteban Echegoyen <echegoyenesteban@gmail.com>
2024-03-30 23:28:53 -0300

    Primera pre entrega Echegoyen

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+import ItemListContainer from "./components/ItemListContainer"
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <ItemListContainer greeting={"Le Lièvre - Paris"}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+function ItemListContainer({greeting}) {
+  return (
+    <h2>
+        <div>
+            {greeting}
+        </div>
+    </h2>
+  )
+}
+
+export default ItemListContainer
